b109fa04652a917c7a80f3b8c5ae03e9b697c78f
Stewart Harper <stewart.harper@gmail.com>
2014-08-19 10:45:04 -0700

    upgraded to wss

--- www/js/main.js
+++ www/js/main.js
@@ -41,7 +41,7 @@ function initialize() {
 	if ("WebSocket" in window) {
 
 		// ============= AIS ====================
-		ws = FMEServer.getWebSocketConnection("twitter-stream-out");
+		ws = FMEServer.getSecureWebSocketConnection("twitter-stream-out");
 
 		// receive
 		ws.onmessage = function (evt) {
