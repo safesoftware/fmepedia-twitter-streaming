704f3d4af3b6a76ee9395bd1f317c19e3fa2b9f6
De Wet van Niekerk <dewert at google's webmail domain>
2014-12-11 15:54:55 -0800

    Updated for 2015

--- www/js/main.js
+++ www/js/main.js
@@ -1,9 +1,11 @@
 function initialize() {
-	// see FMEServer.js for parameter values
-	FMEServer.init({
-		server : "https://fmepedia2014-safe-software.fmecloud.com",
-		token : "8be243c0fc2f5f34977050bdab57ebbdd3e72aa2"
+	$.getJSON("http://demos.fmeserver.com.s3.amazonaws.com/server-demo-config.json", function(config) {
+		FMEServer.init(config.initObject);
+		run();
 	});
+}
+
+function run() {
 
 	var myLatlng = new google.maps.LatLng(17.7850,-12.4183);
 	var light_grey_style = [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}];
